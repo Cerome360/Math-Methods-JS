b6b28e7e67aa75c69e09f8c91393509dc2b7ee4b
Cerome360 <61514307+Cerome360@users.noreply.github.com>
2025-07-07 12:30:09 +0100

    Add files via upload

---
+++ JS5. Math Methods/js/main.js
@@ -0,0 +1,34 @@
+
+// Math Methods and Properties
+console.log(Math.PI);
+
+// Math.trunc() method returns the integer trunc of a number and truncates any decimals
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(6.98736556545));
+
+// Math.round() method Returns the value of a number rounded to the nearest integer
+console.log(Math.round(Math.PI));
+
+// Math.ceil() returns the value of a number rounded up regardless of the decimal. (ceiling(up))
+console.log(Math.ceil(Math.PI));
+
+//Math.floor() method returns the value of a number rounded down regardless of the decimal.(floor(down))
+console.log(Math.floor(6.98736556545));
+
+// Math.pow() method takes 2 arguments, the base and exponential.
+console.log(Math.pow(2, 10));
+
+// Math.min() method returns the smallest number from zero or more numbers
+console.log(Math.min(2, 10, 1, -2));
+
+// Math.max() method returns the largest number from zero or more numbers
+console.log(Math.max(2, 10, 1, -2));
+
+// Math.random() method provides a psuedo-random number from zero to one
+console.log(Math.random());
+
+// JavaScript expression using the random method with others
+console.log(Math.floor(Math.random() * 10) + 1);
+
+
+
